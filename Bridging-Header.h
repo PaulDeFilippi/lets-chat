ece74cf89e7d55e386667ebff7908c0f64768bee
Paul Defilippi <allinfo.pd@gmail.com>
2019-08-21 15:54:48 -0400

    initial setup

---
+++ Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  lets-chat
+//
+//  Created by Paul Defilippi on 8/19/19.
+//  Copyright © 2019 Paul Defilippi. All rights reserved.
+//
+
+#import <JSQMessagesViewController/JSQMessages.h>
